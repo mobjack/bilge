e5cb6907c96c4210162eb2af3edda6f80dd39096
mobjack <ericparker4@live.com>
2021-12-15 23:35:12 +0000

    updated portainer path

--- docker/services/portainer/install_portainer.sh
+++ docker/services/portainer/install_portainer.sh
@@ -6,5 +6,5 @@ docker volume create portainer_data
 docker run -d -p 8000:8000 -p 9443:9443 --name portainer \
     --restart=always \
     -v /var/run/docker.sock:/var/run/docker.sock \
-    -v portainer_data:/data \
+    -v docker_data/portainer:/data \
     cr.portainer.io/portainer/portainer-ce:2.9.3
