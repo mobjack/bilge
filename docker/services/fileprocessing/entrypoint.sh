f0f378f48647fd8bd5df644071ddc35a4c8f9058
mobjack <ericparker4@live.com>
2022-01-24 15:16:03 -0800

    updated entrypoint for restart smb

--- docker/services/fileprocessing/entrypoint.sh
+++ docker/services/fileprocessing/entrypoint.sh
@@ -8,6 +8,15 @@ if [ "$1" = "smbd" ] ; then
     shift
   fi
 
+  # Kill old processes if any
+  smb_count=`/bin/ps | grep smb.conf | grep -v grep | wc -l`
+  if [ $smb_count -gt 0 ] ; then
+      for PID in `ps -ef | grep -v grep | grep smb.conf | awk '{print $2}'`
+      do
+          kill -9 $PID
+      done
+  fi
+
   smbd --foreground --log-stdout --debuglevel=1 --configfile /smb.conf "$@" &
 
   PID=$!
